5371c2983612995034c0c04213ead644f02057b3
stevekelly27 <stevekelly28@hotmail.com>
2022-04-29 13:45:25 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,24 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
-function runGame(){
+function runGame(gameType){
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert('unknown game type: `${gameType}`');
+        throw 'unknown game type: `${gameType}`.aborting';
+    }
 }
 
 function checkAnswer(){
@@ -32,8 +41,10 @@ function incrementWromgAnswer(){
 
 }
 
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion(){
